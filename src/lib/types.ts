ffabda0ea9708d06853ce5971cdc11e1bf41fb2f
Marko Bozic <markobozic346@gmail.com>
2023-09-02 19:40:32 +0200

    Add TodoWithUser type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,5 @@ import { AppRouter } from "@/server/routers/root";
 import { inferRouterOutputs } from "@trpc/server";
 
 export type Todo = inferRouterOutputs<AppRouter>["todo"]["getUserTodos"][0];
+export type TodoWithUser =
+  inferRouterOutputs<AppRouter>["todo"]["getAllUsersTodos"][0];
